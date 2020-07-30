4d677b13c1ad5a5550885530fd826626291c912d
uxchar <c33.j@outlook.com>
2020-07-30 18:13:55 -0400

    more changes

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ wrapperMenu.addEventListener('click', function(){
 function checkEmail() {
 
   var email = document.getElementById('email');
-  var emailTest = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  var emailTest = /^([a-zA-Z0-9@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
   if (!emailTest.test(email.value)) {
   alert('Email address apears invalid, please provide your email address.');
