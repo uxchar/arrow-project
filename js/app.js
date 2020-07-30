73cbb257b2414800802f74fab3e1e3553fba7c68
uxchar <c33.j@outlook.com>
2020-07-30 18:11:22 -0400

    img optimization, css changes

--- js/app.js
+++ js/app.js
@@ -20,55 +20,15 @@ wrapperMenu.addEventListener('click', function(){
 
 
 
-/*Image Animation*/
-
-
-var scroll = window.requestAnimationFrame ||
-            function(callback){ window.setTimeout(callback, 1000/60)};
-var elementsToShow = document.querySelectorAll('.show-on-scroll');
-
-function loop() {
-
-  elementsToShow.forEach(function (element) {
-    if (isElementInViewport(element)) {
-      element.classList.add('is-visible');
-    } else {
-      element.classList.remove('is-visible');
-    }
-  });
-
-  scroll(loop);
-}
-
-loop();
-
-function isElementInViewport(el) {
-  if (typeof jQuery === "function" && el instanceof jQuery) {
-    el = el[0];
-  }
-  var rect = el.getBoundingClientRect();
-  return (
-    (rect.top <= 0
-      && rect.bottom >= 0)
-    ||
-    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-    ||
-    (rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-  );
-}
-
-
 /* Email validation*/
 
 function checkEmail() {
 
   var email = document.getElementById('email');
-  var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  var emailTest = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
-  if (!filter.test(email.value)) {
-  alert('Please provide a valid email address');
+  if (!emailTest.test(email.value)) {
+  alert('Email address apears invalid, please provide your email address.');
   email.focus;
   return false;
 }
